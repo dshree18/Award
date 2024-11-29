2bcbc80e8f62d0c0d50a0649c5c0e6a32505ec1a
dshree18 <dhanya.vks18@gmail.com>
2024-11-29 09:46:02 +0530

    Update awardRoute.js

--- routes/awardRoute.js
+++ routes/awardRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createAward } from "../controllers/awardController.js"; // Correct import path
+import { createAward, getAwardsone, getAwardsall, deleteAward, updateAward } from "../controllers/awardController.js"; // Correctly import the functions
 
 const route = express.Router();
 
-// Define the route for creating awards
-route.post("/create", createAward);
-
-// Export the route
+// Define the routes for creating, getting, and deleting awards
+route.post("/create", createAward);  // Route to create an award
+route.get("/ondu", getAwardsone);     // Route to get awards (this can be customized based on your needs)
+route.delete("/delete", deleteAward); // Route to delete an award
+route.get("/yella",getAwardsall);
+route.put("/changemaadu",updateAward)
+// Export the routes
 export default route;
